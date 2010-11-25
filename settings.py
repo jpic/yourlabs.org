9af2620ffe31fe0f8114d9e4edbc7475089bcad6
James Pic <jamespic@gmail.com>
2010-11-25 10:35:20 +0100

    Added announcements app and did a stupid cosmetic change

--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@ PINAX_ROOT = os.path.abspath(os.path.dirname(pinax.__file__))
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 # tells Pinax to use the default theme
-PINAX_THEME = "default"
+PINAX_THEME = 'default'
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -18,23 +18,23 @@ TEMPLATE_DEBUG = DEBUG
 SERVE_MEDIA = DEBUG
 
 INTERNAL_IPS = [
-    "127.0.0.1",
+    '127.0.0.1',
 ]
 
 ADMINS = [
-    # ("Your Name", "your_email@domain.com"),
+    # ('Your Name', 'your_email@domain.com'),
 ]
 
 MANAGERS = ADMINS
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3", # Add "postgresql_psycopg2", "postgresql", "mysql", "sqlite3" or "oracle".
-        "NAME": "dev.db",                       # Or path to database file if using sqlite3.
-        "USER": "",                             # Not used with sqlite3.
-        "PASSWORD": "",                         # Not used with sqlite3.
-        "HOST": "",                             # Set to empty string for localhost. Not used with sqlite3.
-        "PORT": "",                             # Set to empty string for default. Not used with sqlite3.
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'dev.db',                       # Or path to database file if using sqlite3.
+        'USER': '',                             # Not used with sqlite3.
+        'PASSWORD': '',                         # Not used with sqlite3.
+        'HOST': '',                             # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                             # Set to empty string for default. Not used with sqlite3.
     }
 }
 
@@ -43,11 +43,11 @@ DATABASES = {
 # although not all choices may be available on all operating systems.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = "Europe/Paris"
+TIME_ZONE = 'Europe/Paris'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
 
@@ -56,73 +56,75 @@ SITE_ID = 1
 USE_I18N = True
 
 # Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, "site_media", "media")
+# Example: '/home/media/media.lawrence.com/'
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'site_media', 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = "/site_media/media/"
+# Examples: 'http://media.lawrence.com', 'http://example.com/media/'
+MEDIA_URL = '/site_media/media/'
 
 # Absolute path to the directory that holds static files like app media.
-# Example: "/home/media/media.lawrence.com/apps/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, "site_media", "static")
+# Example: '/home/media/media.lawrence.com/apps/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'site_media', 'static')
 
 # URL that handles the static files like app media.
-# Example: "http://media.lawrence.com"
-STATIC_URL = "/site_media/static/"
+# Example: 'http://media.lawrence.com'
+STATIC_URL = '/site_media/static/'
 
 # Additional directories which hold static files
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, "media"),
-    os.path.join(PINAX_ROOT, "media", PINAX_THEME),
+    os.path.join(PROJECT_ROOT, 'media'),
+    os.path.join(PINAX_ROOT, 'media', PINAX_THEME),
 ]
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = posixpath.join(STATIC_URL, "admin/")
+# Examples: 'http://foo.com/media/', '/media/'.
+ADMIN_MEDIA_PREFIX = posixpath.join(STATIC_URL, 'admin/')
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = "$)#&dm!m2&y(a@0fzyf5yo-r5s27e=lm17$+ni==gwp$nef*p3"
+SECRET_KEY = '$)#&dm!m2&y(a@0fzyf5yo-r5s27e=lm17$+ni==gwp$nef*p3'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = [
-    "django.template.loaders.filesystem.load_template_source",
-    "django.template.loaders.app_directories.load_template_source",
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
 ]
 
 MIDDLEWARE_CLASSES = [
-    "django.middleware.common.CommonMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "pinax.middleware.security.HideSensistiveFieldsMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
-    "pagination.middleware.PaginationMiddleware",
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'pinax.middleware.security.HideSensistiveFieldsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'pagination.middleware.PaginationMiddleware',
 ]
 
-ROOT_URLCONF = "main.urls"
+ROOT_URLCONF = 'main.urls'
 
 TEMPLATE_DIRS = [
-    os.path.join(PROJECT_ROOT, "templates"),
-    os.path.join(PINAX_ROOT, "templates", PINAX_THEME),
+    os.path.join(PROJECT_ROOT, 'templates'),
+    os.path.join(PINAX_ROOT, 'templates', PINAX_THEME),
 ]
 
 TEMPLATE_CONTEXT_PROCESSORS = [
-    "django.core.context_processors.auth",
-    "django.core.context_processors.debug",
-    "django.core.context_processors.i18n",
-    "django.core.context_processors.media",
-    "django.core.context_processors.request",
-    "django.contrib.messages.context_processors.messages",
+    'django.core.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.request',
+    'django.contrib.messages.context_processors.messages',
     
-    "staticfiles.context_processors.static_url",
+    'staticfiles.context_processors.static_url',
     
-    "pinax.core.context_processors.pinax_settings",
-    "planet.context_processors.context",
-    "context_processors.global_context",
+    'pinax.core.context_processors.pinax_settings',
+    'planet.context_processors.context',
+    'context_processors.global_context',
+    'multilingual.context_processors.multilingual',
+    'announcements.context_processors.site_wide_announcements',
 
 ]
 
@@ -132,37 +134,38 @@ INSTALLED_APPS = [
     'admin_tools.menu',
     'admin_tools.dashboard',
     # Django
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.sites",
-    "django.contrib.messages",
-    "django.contrib.humanize",
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.humanize',
     
-    "pinax.templatetags",
-    "pinax.apps.account",
-    "emailconfirmation",
+    'pinax.templatetags',
+    'pinax.apps.account',
+    'emailconfirmation',
     
     # external
-    "staticfiles",
-    "debug_toolbar",
+    'staticfiles',
+    'debug_toolbar',
     
     # Pinax
     
     # project
-    "django_extensions",
-    "planet",
-    "tagging",
-    "pagination",
-    "fanzine",
-
+    'django_extensions',
+    'planet',
+    'tagging',
+    'pagination',
+    'fanzine',
+    'multilingual',
+    'announcements',
 ]
 
 # planet
-LANGUAGE_COOKIE_NAME = "planetlng"
-SESSION_COOKIE_NAME = "planetid"
-USER_AGENT = "django-planet/0.1"
+LANGUAGE_COOKIE_NAME = 'planetlng'
+SESSION_COOKIE_NAME = 'planetid'
+USER_AGENT = 'django-planet/0.1'
 
 LANGUAGES = (
     ('en', u'English'),
@@ -172,15 +175,15 @@ LANGUAGES = (
 DEFAULT_LANGUAGE = 1
 
 FIXTURE_DIRS = [
-    os.path.join(PROJECT_ROOT, "fixtures"),
+    os.path.join(PROJECT_ROOT, 'fixtures'),
 ]
 
-MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
-EMAIL_BACKEND = "mailer.backend.DbBackend"
+EMAIL_BACKEND = 'mailer.backend.DbBackend'
 
 DEBUG_TOOLBAR_CONFIG = {
-    "INTERCEPT_REDIRECTS": False,
+    'INTERCEPT_REDIRECTS': False,
 }
 
 # local_settings.py can be used to override environment-specific settings
