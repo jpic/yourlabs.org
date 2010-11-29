863a8aa50385b2c486a5a99412c0ec102ec2f3dc
James Pic <jamespic@gmail.com>
2010-11-29 13:36:27 +0100

    FanzinePartPost should update its Post.tags with its FanzinePart.tag

--- context_processors.py
+++ context_processors.py
@@ -1,10 +1,28 @@
 from datetime import date
+from datetime import datetime
 
 from django.utils.translation import ugettext as _
 
 from fanzine.models import *
+from pinax.apps.blog.models import *
 
 from forms import SubscriptionForm
+
+
+def previous_posts(request):
+    context = {}
+    qs = Post.objects.filter(publish__lte=datetime.now())
+    qs = qs.select_related()
+    context['previous_posts'] = qs.all()[:7]
+    return context
+
+def next_posts(request):
+    context = {}
+    qs = Post.objects.filter(publish__gt=datetime.now())
+    qs = qs.select_related()
+    if request.user.is_staff and qs.count():
+        context['next_posts'] = qs.all()[:7]
+    return context
 
 def global_context(request):
     context = {}
